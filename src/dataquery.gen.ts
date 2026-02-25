7053d114b43c0032e9616ef07653f37b52c4800b
Adam Yeats <adam.yeats@grafana.com>
2026-02-25 15:29:28 +0000

    Elasticsearch: Refactor to use a unified query field with a `queryType` property (#118058)
    Refactor to use a unified query field with a queryType property

--- src/dataquery.gen.ts
+++ src/dataquery.gen.ts
@@ -10,6 +10,8 @@
 
 import * as common from '@grafana/schema';
 
+export type QueryType = ('lucene' | 'dsl');
+
 export type BucketAggregation = (DateHistogram | Histogram | Terms | Filters | GeoHashGrid | Nested);
 
 export type MetricAggregation = (Count | PipelineMetricAggregation | MetricAggregationWithSettings);
@@ -394,13 +396,17 @@ export interface ElasticsearchDataQuery extends common.DataQuery {
    */
   metrics?: MetricAggregation[];
   /**
-   * Lucene query
+   * Query string (Lucene or DSL depending on queryType)
    */
   query?: string;
   /**
-   * Raw DSL query
+   * Specify the query flavor
+   * TODO make this required and give it a default
    */
-  rawDSLQuery?: string;
+  /**
+   * Query type - determines how the query field is interpreted
+   */
+  queryType?: string;
   /**
    * Name of time field
    */
