d5c3cb1b564a99412830053865913a5f5f8551c6
Nathan Vērzemnieks <njvrzm@gmail.com>
2026-04-07 23:51:55 +0200

    Build: override plugin sdk's BuildAll to enable extra platforms (#245)
    * Build: override plugin sdk's BuildAll to enable extra platforms
    
    * Add Test() and TestRace() wrappers
    
    ---------
    
    Co-authored-by: Ryan Morris <ryan.morris@grafana.com>

--- Magefile.go
+++ Magefile.go
@@ -4,17 +4,17 @@
 package main
 
 import (
-	// mage:import
 	build "github.com/grafana/grafana-plugin-sdk-go/build"
 	"github.com/magefile/mage/mg"
 )
 
 // Default configures the default target.
-var Default = BuildAllPlus
+var Default = BuildAll
 
-// BuildAllPlus builds all default targets plus linux/s390x and windows/arm64,
-// to support bundling with Grafana builds on those platforms.
-func BuildAllPlus() {
+// BuildAll builds all default targets plus linux/s390x and windows/arm64,
+// to support bundling with Grafana builds on those platforms. This overrides
+// the buildAll from grafana-plugin-sdk-go.
+func BuildAll() {
 	b := build.Build{}
 	mg.Deps(b.Linux, b.Windows, b.Darwin, b.DarwinARM64, b.LinuxARM64, b.LinuxARM, LinuxS390X, WindowsARM64)
 }
@@ -27,4 +27,16 @@ func LinuxS390X() error {
 // WindowsARM64 builds the back-end plugin for Windows on arm64.
 func WindowsARM64() error {
 	return build.Build{}.Custom("windows", "arm64")
+}
+
+// Test() wraps the plugin SDK's Test to make it accessible since we're
+// no longer using mage:import
+func Test() error {
+	return build.Test()
+}
+
+// TestRace() wraps the plugin SDK's Test to make it accessible since we're
+// no longer using mage:import
+func TestRace() error {
+	return build.TestRace()
 }
