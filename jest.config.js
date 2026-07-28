b4b296ee09fdec44124f2dfcc136412ff41a10bb
Andreas Christou <andreas.christou@grafana.com>
2026-07-28 12:43:24 +0100

    Bump @grafana/* to 13.1.1 and update dependencies (#404)
    - Bump @grafana/* runtime packages to 13.1.1, plugin-ui to 0.17.3, react-use to 17.6.1
    - Refresh package-lock.json to latest compatible transitive versions
    - Remove now-redundant @grafana/plugin-ui override
    - Extend jest transformIgnorePatterns in jest.config.js for @marcbachmann/cel-js (required by plugin-ui 0.17)
    - Bump backend Go modules to latest compatible releases

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,11 @@
 // generally used by snapshots, but can affect specific tests
 process.env.TZ = 'UTC';
 
+const { grafanaESModules, nodeModulesToTransform } = require('./.config/jest/utils');
+
 module.exports = {
   // Jest configuration provided by Grafana scaffolding
   ...require('./.config/jest.config'),
+  // @grafana/plugin-ui ships @marcbachmann/cel-js as ESM, which jest must transform
+  transformIgnorePatterns: [nodeModulesToTransform([...grafanaESModules, '@marcbachmann/cel-js'])],
 };
