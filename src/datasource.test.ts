d5de7d05ffe2154f5a7c91cfab3b9f82130461de
Adam Yeats <adam.yeats@grafana.com>
2026-07-30 21:46:19 +0100

    fix: pass factory functions to rxjs throwError in datasource tests

--- src/datasource.test.ts
+++ src/datasource.test.ts
@@ -1658,7 +1658,7 @@ describe('ElasticDatasource', () => {
             data: {},
           });
         }
-        return throwError({ status: 500 });
+        return throwError(() => ({ status: 500 }));
       });
 
       const timeRange = { from: 1, to: 2 } as unknown as TimeRange;
@@ -1675,7 +1675,7 @@ describe('ElasticDatasource', () => {
       });
 
       ds.getResource = jest.fn().mockImplementation(() => {
-        return throwError({ status: 404 });
+        return throwError(() => ({ status: 404 }));
       });
 
       const timeRange = createTimeRange(dateTime().subtract(2, 'week'), dateTime());
